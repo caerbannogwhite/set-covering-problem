4b7515ce6697abf55f259e56a18a8049bff9e523
Massimo Meneghello <massimo.meneghello93@gmail.com>
2020-03-19 16:31:04 +0000

    balas dense: dual 3 and separation procedure working

--- src/balas_dense.hpp
+++ src/balas_dense.hpp
@@ -3,7 +3,7 @@
 
 #include "balas_common.hpp"
 
-int baldns_separation_proc(arma::mat &mat, arma::vec &x, arma::vec &s, double zUpp, double zLow, std::set<int> &wSet);
+int baldns_separation_proc(arma::mat &mat, arma::vec &x, arma::vec &s, std::unordered_set<int> &tSet, std::unordered_set<int> &wSet, double zUpp, double zLow);
 int baldns_branch_rule1(arma::mat &mat, arma::vec &x, arma::vec &s, double zUpp, double zLow, std::vector<std::set<int>> branchSet, const int maxBranch, const int maxSingl);
 int baldns_branch_rule1_test(arma::mat &mat, arma::vec &x, arma::vec &s, double zUpp, double zLow, std::vector<std::unordered_set<int>> branchSet, const int maxBranch, const int maxSingl);
 
@@ -15,7 +15,7 @@ double baldns_heur_primal_12(arma::mat &mat, arma::vec &obj, arma::vec &x);
 double baldns_heur_primal_5b(arma::mat &mat, arma::vec &obj, arma::vec &x, arma::vec &s, arma::vec &u);
 
 bool baldns_is_dual_sol(arma::mat &mat, arma::vec &obj, arma::vec &u);
-double baldns_heur_dual_1(arma::mat &mat, arma::vec &x, arma::vec &u, arma::vec &s);
-double baldns_heur_dual_3(arma::mat &mat, arma::vec &x, arma::vec &u, arma::vec &s, const double zUpp);
+double baldns_heur_dual_1(arma::mat &mat, arma::vec &x, arma::vec &u, arma::vec &s, std::unordered_set<int> &tSet);
+double baldns_heur_dual_3(arma::mat &mat, arma::vec &x, arma::vec &u, arma::vec &s, std::unordered_set<int> &tSet, const double zUpp, double zLow);
 
 #endif //BALAS_DENSE_H
--- src/balas_solver.cpp
+++ src/balas_solver.cpp
@@ -4,39 +4,62 @@
 
 STATUS balsol(BALSOLEnv &inst)
 {
+    int cutSize;
     double uppVal;
     double lowVal;
+
+    std::unordered_set<int> tSet = std::unordered_set<int>();
+    std::unordered_set<int> wSet = std::unordered_set<int>();
 
     //////////////////////////////      DENSE       //////////////////////////
     balcomm_read_instance_dns(inst);
 
     arma::vec x = arma::vec(inst.dnsmat.n_cols);
+    arma::vec p = arma::vec(inst.dnsmat.n_rows);
     arma::vec u = arma::vec(inst.dnsmat.n_rows);
     arma::vec s;
 
-    std::set<int> w = std::set<int>();
 
     std::cout << "BALAS SOLVER - DENSE" << std::endl;
 
     uppVal = baldns_heur_primal_0(inst.dnsmat, inst.dnsobj, x, 3);
+    std::cout << "\n--------        primal 0        --------\n";
     std::cout << "is prim sol = " << baldns_is_cover(inst.dnsmat, x) << std::endl;
     std::cout << "prim val = " << uppVal << std::endl;
 
+    // intialize T(x) set
+    p = inst.dnsmat * x;
+    for (int i = inst.dnsmat.n_rows; i--;)
+    {
+        if (fabs(p(i) - 1.0) < SC_EPSILON_SMALL)
+        {
+            tSet.insert(i);
+        }
+    }
+
     s = arma::vec(inst.dnsobj);
-    lowVal = baldns_heur_dual_1(inst.dnsmat, x, u, s);
+    lowVal = baldns_heur_dual_1(inst.dnsmat, x, u, s, tSet);
+    std::cout << "\n--------        dual 1        --------\n";
+    std::cout << "is dual sol = " << baldns_is_dual_sol(inst.dnsmat, inst.dnsobj, u) << std::endl;
+    std::cout << "dual val = " << lowVal << std::endl;
+
+    lowVal = baldns_heur_dual_3(inst.dnsmat, x, u, s, tSet, uppVal, lowVal);
+    std::cout << "\n--------        dual 3        --------\n";
     std::cout << "is dual sol = " << baldns_is_dual_sol(inst.dnsmat, inst.dnsobj, u) << std::endl;
     std::cout << "dual val = " << lowVal << std::endl;
 
     // generate cut
-    baldns_separation_proc(inst.dnsmat, x, s, uppVal, lowVal, w);
-    std::cout << "w =\n";
-    for (auto it = w.cbegin(); it != w.cend(); ++it)
-    {
-        std::cout << *it << std::endl;
-    }
+    cutSize = baldns_separation_proc(inst.dnsmat, x, s, tSet, wSet, uppVal, lowVal);
+    std::cout << "\n--------        separation        --------\n";
+    std::cout << "w size = " << cutSize << std::endl;
+    //std::cout << "w =\n"; for (auto it = wSet.cbegin(); it != wSet.cend(); ++it) { std::cout << *it << std::endl; }
 
 
     //////////////////////////////      SPARSE       //////////////////////////
+
+    tSet.clear();
+    wSet.clear();
+
     balcomm_read_instance_spr(inst);
 
     arma::sp_mat xSpr = arma::sp_mat(inst.sprmat.n_cols, 1);
